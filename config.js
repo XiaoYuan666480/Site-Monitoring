2ce9fd79c21fb0bd8d2440c5bfaa5d033650fe2f
小袁同学 <85924672+XiaoYuan666480@users.noreply.github.com>
2023-04-14 21:44:33 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Site Monitoring',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    '请填写您的API，如需增加复制粘贴一行即可',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://www.xiaoyuantongxue.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/XiaoYuan666480'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.xiaoyuantongxue.cn'
+    },
+  ],
+};
